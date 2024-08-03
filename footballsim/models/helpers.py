aaf18a8ef6651c4740d26ace09e264e06326f0b2
Fady Emad <fadyemad14705@gmail.com>
2024-08-03 14:47:12 +0300

    improve: standings

--- footballsim/models/helpers.py
+++ footballsim/models/helpers.py
@@ -1,6 +1,10 @@
 import math
+from typing import TYPE_CHECKING
 
 from .constants import XG_CONSTANT, XG_FACTOR
+
+if TYPE_CHECKING:
+    from .standings import TeamStanding
 
 
 def calculate_xg(strength_diff: float) -> float:
@@ -13,3 +17,7 @@ def calculate_strength_diff(xg: float) -> float:
     """
 
     return math.log(xg / XG_CONSTANT, XG_FACTOR)
+
+
+def default_tie_breakers(standing: "TeamStanding") -> tuple[int, ...]:
+    return standing.points, standing.goals_difference, standing.goals_scored
--- footballsim/models/standings.py
+++ footballsim/models/standings.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
-from typing import Annotated
+from typing import Annotated, Callable
 
 from annotated_types import Len
 from pydantic import BaseModel, ConfigDict, computed_field
 from tabulate import tabulate
 
 from .constants import DRAW_POINTS, WIN_POINTS
+from .helpers import default_tie_breakers
 from .match import Match
 from .team import Team
 
@@ -14,6 +15,7 @@ class TeamStanding(BaseModel):
     model_config = ConfigDict(frozen=True)
 
     team: Team
+    position: int = 1
     wins: int = 0
     draws: int = 0
     losses: int = 0
@@ -36,7 +38,7 @@ class TeamStanding(BaseModel):
         return WIN_POINTS * self.wins + DRAW_POINTS * self.draws
 
     def __lt__(self, other: "TeamStanding") -> bool:
-        return self._weights() < other._weights()
+        return self.position < other.position
 
     def __add__(self, other: "TeamStanding") -> "TeamStanding":
         assert self.team is other.team
@@ -50,22 +52,44 @@ class TeamStanding(BaseModel):
             }
         )
 
-    def _weights(self) -> tuple[int, ...]:
-        return self.points, self.goals_difference, self.goals_scored
-
 
 class Standings(BaseModel):
     model_config = ConfigDict(frozen=True)
 
     team_standings: Annotated[list[TeamStanding], Len(min_length=2)]
+    tie_breakers: Callable[[TeamStanding], tuple[int, ...]] = default_tie_breakers
 
     @classmethod
     def from_teams(cls, teams: list[Team]) -> "Standings":
-        return cls(team_standings=[TeamStanding(team=team) for team in teams])
+        return cls(
+            team_standings=[
+                TeamStanding(team=team, position=len(teams)) for team in teams
+            ]
+        )
+
+    @staticmethod
+    def calculate_teams_delta(match: Match) -> tuple[TeamStanding, TeamStanding]:
+        """Calculate the delta TeamStanding of the home team and away team from a single match and returns the results in the order: home, away"""
+        assert bool(match.result)
+        home: dict[str, int] = {}
+        away: dict[str, int] = {}
+        if match.is_draw():
+            home["draws"] = away["draws"] = 1
+        elif match.is_win():
+            home["wins"] = away["losses"] = 1
+        elif match.is_loss():
+            home["losses"] = away["wins"] = 1
+
+        home["goals_scored"] = away["goals_conceded"] = match.result.home_goals
+        home["goals_conceded"] = away["goals_scored"] = match.result.away_goals
+        return TeamStanding(team=match.home_team, **home), TeamStanding(
+            team=match.away_team, **away
+        )
 
     def __str__(self) -> str:
         data = [
             [
+                s.position,
                 s.team,
                 s.matches_played,
                 s.wins,
@@ -80,8 +104,19 @@ class Standings(BaseModel):
         ]
         return tabulate(
             data,
-            headers=["TEAM", "MP", "W", "D", "L", "GS", "GC", "GD", "PTS"],
+            headers=["", "TEAM", "MP", "W", "D", "L", "GS", "GC", "GD", "PTS"],
         )
+
+    def _calculate_positions(
+        self, team_standings: list[TeamStanding]
+    ) -> list[TeamStanding]:
+        """Calculate positions of a list of team standings using the standings' tie breaker gunction"""
+        return [
+            s.model_copy(update={"position": i + 1})
+            for i, s in enumerate(
+                sorted(team_standings, key=self.tie_breakers, reverse=True)
+            )
+        ]
 
     def update_from_matches(self, matches: list[Match]) -> "Standings":
         """Returns new standings same as current standings but with statistics updated according to results from given matches"""
@@ -89,29 +124,9 @@ class Standings(BaseModel):
         for match in matches:
             if not match.result:
                 continue
-
-            home_update: dict[str, int] = {}
-            away_update: dict[str, int] = {}
-            if match.is_draw():
-                home_update["draws"] = away_update["draws"] = 1
-            elif match.is_win():
-                home_update["wins"] = away_update["losses"] = 1
-            elif match.is_loss():
-                home_update["losses"] = away_update["wins"] = 1
-
-            home_update["goals_scored"] = away_update["goals_conceded"] = (
-                match.result.home_goals
-            )
-            home_update["goals_conceded"] = away_update["goals_scored"] = (
-                match.result.away_goals
-            )
-
-            deltas[match.home_team].append(
-                TeamStanding(team=match.home_team, **home_update)
-            )
-            deltas[match.away_team].append(
-                TeamStanding(team=match.away_team, **away_update)
-            )
+            home, away = Standings.calculate_teams_delta(match)
+            deltas[match.home_team].append(home)
+            deltas[match.away_team].append(away)
 
         team_standings: list[TeamStanding] = []
         for standing in self.team_standings:
@@ -119,5 +134,5 @@ class Standings(BaseModel):
             team_standings.append(new_standing)
 
         return self.model_copy(
-            update={"team_standings": sorted(team_standings, reverse=True)}
+            update={"team_standings": self._calculate_positions(team_standings)}
         )
