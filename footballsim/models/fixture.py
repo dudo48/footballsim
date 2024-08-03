486282558678cada9a78674a34c6adb78bc36ab5
Fady Emad <fadyemad14705@gmail.com>
2024-08-03 23:46:32 +0300

    improve league

--- footballsim/models/fixture.py
+++ footballsim/models/fixture.py
@@ -1,10 +1,13 @@
+import random
 from typing import Annotated
 
 from annotated_types import Len
+from more_itertools import always_reversible, divide
 from pydantic import BaseModel, ConfigDict
 from tabulate import tabulate
 
 from .match import Match
+from .team import Team
 
 
 class Fixture(BaseModel):
@@ -13,6 +16,30 @@ class Fixture(BaseModel):
     model_config = ConfigDict(frozen=True)
 
     matches: Annotated[list[Match], Len(min_length=1)]
+
+    @classmethod
+    def from_teams(
+        cls,
+        teams: list[Team | None],
+        home_or_away: bool = False,
+        shuffle_matches: bool = False,
+    ) -> "Fixture":
+        """Create matches fixture from a list of teams, where the ith team plays with the n - i team, where n is the number of teams.
+        the list of teams might include None values which indicate a 'bye' contestant"""
+        matches: list[Match] = []
+        half_1, half_2 = divide(2, teams)
+        for t1, t2 in zip(half_1, always_reversible(half_2)):
+            if not t1 or not t2:
+                continue
+
+            match = Match(home_team=t1, away_team=t2)
+            if home_or_away and random.random() < 0.5:
+                match = match.get_away_match()
+            matches.append(match)
+
+        if shuffle_matches:
+            random.shuffle(matches)
+        return cls(matches=matches)
 
     def __str__(self) -> str:
         data = [[m.home_team, m.result or "-", m.away_team] for m in self.matches]
--- footballsim/models/league.py
+++ footballsim/models/league.py
@@ -3,7 +3,7 @@ from functools import cached_property
 from typing import Annotated
 
 from annotated_types import Len
-from more_itertools import always_reversible, circular_shifts, divide, nth, padded
+from more_itertools import circular_shifts, flatten, interleave_longest, padded
 from pydantic import BaseModel, ConfigDict, PositiveInt, computed_field
 
 from .fixture import Fixture
@@ -18,19 +18,6 @@ class League(BaseModel):
     teams: Annotated[list[Team], Len(min_length=2)]
     iterations: PositiveInt = 2
 
-    @staticmethod
-    def create_fixture(teams: list[Team | None]) -> Fixture:
-        matches: list[Match] = []
-        half_1, half_2 = divide(2, teams)
-        for t1, t2 in zip(half_1, always_reversible(half_2)):
-            if not t1 or not t2:
-                continue
-            match = Match(home_team=t1, away_team=t2)
-            if random.random() < 0.5:
-                match = match.get_away_match()
-            matches.append(match)
-        return Fixture(matches=matches)
-
     @computed_field
     @cached_property
     def fixtures(self) -> list[Fixture]:
@@ -39,14 +26,18 @@ class League(BaseModel):
         teams = list(padded(self.teams, n=2, fillvalue=None, next_multiple=True))
         random.shuffle(teams)
         fixtures = [
-            League.create_fixture([teams[0]] + list(shifts))
+            Fixture.from_teams(
+                [teams[0]] + list(shifts), home_or_away=True, shuffle_matches=True
+            )
             for shifts in circular_shifts(teams[1:])
         ]
 
         # create all next iterations with alternating home / away teams setting
-        previous_iteration = fixtures
+        previous_iteration = fixtures.copy()
         for _ in range(self.iterations - 1):
-            next_iteration = [f.get_away_fixture() for f in previous_iteration]
+            next_iteration = list(
+                map(lambda f: f.get_away_fixture(), previous_iteration)
+            )
             fixtures.extend(next_iteration)
             previous_iteration = next_iteration
 
@@ -57,10 +48,15 @@ class League(BaseModel):
     def standings(self) -> list[Standings]:
         return [Standings.from_teams(self.teams)]
 
+    @property
+    def matches(self) -> list[Match]:
+        return list(flatten(f.matches for f in self.fixtures))
+
     def __str__(self) -> str:
-        result = ""
-        for i, fixture in enumerate(self.fixtures):
-            result += f"{nth(self.standings, i, default="")}\n\n"
-            result += f"FIXTURE {i + 1}\n{fixture}\n\n"
-        result += str(nth(self.standings, len(self.fixtures), default=""))
-        return result
+        return "\n\n".join(
+            interleave_longest(
+                (str(s) for s in self.standings),
+                (f"FIXTURE {i + 1}" for i in range(len(self.fixtures))),
+                (str(f) for f in self.fixtures),
+            )
+        )
