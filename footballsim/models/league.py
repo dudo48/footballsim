576e5dd71c94122a0c165a772c2089d1d090ed4d
Fady Emad <fadyemad14705@gmail.com>
2024-08-03 15:36:16 +0300

    feat: support odd number of teams for league

--- footballsim/models/league.py
+++ footballsim/models/league.py
@@ -2,8 +2,8 @@ import random
 from functools import cached_property
 from typing import Annotated
 
-from annotated_types import Len, Predicate
-from more_itertools import always_reversible, circular_shifts, divide, nth
+from annotated_types import Len
+from more_itertools import always_reversible, circular_shifts, divide, nth, padded
 from pydantic import BaseModel, ConfigDict, PositiveInt, computed_field
 
 from .match import Match
@@ -15,41 +15,40 @@ from .team import Team
 class League(BaseModel):
     model_config = ConfigDict(frozen=True)
 
-    teams: Annotated[
-        list[Team], Len(min_length=2), Predicate(lambda t: len(t) % 2 == 0)
-    ]
+    teams: Annotated[list[Team], Len(min_length=2)]
     iterations: PositiveInt = 2
+
+    @staticmethod
+    def create_round(teams: list[Team | None]) -> Round:
+        matches: list[Match] = []
+        half_1, half_2 = divide(2, teams)
+        for t1, t2 in zip(half_1, always_reversible(half_2)):
+            if not t1 or not t2:
+                continue
+            match = Match(home_team=t1, away_team=t2)
+            if random.random() < 0.5:
+                match = match.get_away_match()
+            matches.append(match)
+        return Round(matches=matches)
 
     @computed_field
     @cached_property
     def rounds(self) -> list[Round]:
-        def create_round(teams: list[Team]) -> Round:
-            matches: list[Match] = []
-            half_1, half_2 = divide(2, teams)
-            for t1, t2 in zip(half_1, always_reversible(half_2)):
-                match = Match(home_team=t1, away_team=t2)
-                if random.random() < 0.5:
-                    match = match.get_away_match()
-                matches.append(match)
-            return Round(matches=matches)
-
         # create first iteration
-        teams = [t for t in self.teams]
+        # add a dummy team if number of teams is not even
+        teams = list(padded(self.teams, n=2, fillvalue=None, next_multiple=True))
         random.shuffle(teams)
         rounds = [
-            create_round([teams[0]] + list(shifts))
+            League.create_round([teams[0]] + list(shifts))
             for shifts in circular_shifts(teams[1:])
         ]
 
         # create all next iterations with alternating home / away teams setting
-        previous_iteration = [r for r in rounds]
-        next_iteration: list[Round] = []
+        previous_iteration = rounds
         for _ in range(self.iterations - 1):
-            for round in previous_iteration:
-                next_iteration.append(round.get_away_round())
+            next_iteration = [r.get_away_round() for r in previous_iteration]
             rounds.extend(next_iteration)
             previous_iteration = next_iteration
-            next_iteration = []
 
         return rounds
 
